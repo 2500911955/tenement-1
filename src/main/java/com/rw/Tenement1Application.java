6d4cdc542d7155bfb7ac68d06fc2fbb68132c865
c语言 <c@Lenovo-PC>
2018-08-31 10:34:35 +0800

    first

---
+++ src/main/java/com/rw/Tenement1Application.java
@@ -0,0 +1,12 @@
+package com.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tenement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tenement1Application.class, args);
+	}
+}
